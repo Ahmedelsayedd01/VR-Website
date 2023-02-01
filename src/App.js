dca78c02b348bad6e18b9b446acb360a5aa1612a
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-01 23:59:44 +0200

    New Update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "./components/Banner";
+import Header from "./components/Header";
+import NavMobile from "./components/NavMobile";
 
 // import Components
 
 const App = () => {
+  const [navMobile, setNavMobile] = useState(true);
   return (
     <div>
+      <Header aetNavMobile={setNavMobile} />
       <Banner />
+      {/* mobile nav */}
+      <div
+        className={`${
+          navMobile ? "right-0" : "-right-full"
+        } fixed top-0 bottom-0 w-48 transition-all`}
+      >
+        <NavMobile aetNavMobile={setNavMobile}/>
+      </div>
     </div>
   );
 };
