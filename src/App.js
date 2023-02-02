7202d5b191a69c1870a944d1b1ea5a08a62bedfe
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-03 01:05:30 +0200

    New Update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import React, { useState } from "react";
+
+// import Components
 import Banner from "./components/Banner";
 import Header from "./components/Header";
 import NavMobile from "./components/NavMobile";
-
-// import Components
+//import Icon
+import { HiMenu } from "react-icons/hi";
+import { IoClose } from "react-icons/io5";
 
 const App = () => {
   const [navMobile, setNavMobile] = useState(true);
   return (
     <div>
-      <Header etNavMobile={setNavMobile} />
+      <div className="relative container mx-auto">
+        <Header />
+        {/* nav mobile btn open */}
+        <HiMenu
+          onClick={() => setNavMobile(!false)}
+          className="lg:hidden absolute top-[33%] right-[4%]
+          text-3xl text-white cursor-pointer"
+          />
+      </div>
+
       <Banner />
+
       {/* mobile nav */}
       <div
         className={`${
           navMobile ? "right-0" : "-right-full"
         } fixed top-0 bottom-0 w-48 transition-all`}
       >
-        <NavMobile etNavMobile={setNavMobile} />
+        {/* nav mobile btn close */}
+        <IoClose
+          onClick={() => {
+            setNavMobile(false);
+          }}
+      className="text-3xl absolute left-4
+      top-6 cursor-pointer"
+        />
+        <NavMobile />
       </div>
     </div>
   );
