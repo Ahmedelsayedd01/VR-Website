cc1a011eaf136fddcb5969f60b24dc166b7f8185
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-03 01:05:47 +0200

    New Update

--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -6,11 +6,10 @@ const NavMobile = ({ setNavMobile }) => {
   
   return (
     <nav className="lg:hidden bg-[#251f3f] h-full">
-      <IoClose
-        onClick={() => setNavMobile(true)}
-        className="text-3xl absolute left-4
-      top-6 cursor-pointer"
-      />
+      
+
+
+      
       <ul
         className="flex flex-col items-center 
         justify-center space-y-8 h-full
