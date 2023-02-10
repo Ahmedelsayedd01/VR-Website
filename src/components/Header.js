7a060f28d7759997878a1001df1deec657babfa5
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-10 17:09:07 +0200

    New Update

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,12 @@ import Nav from "./Nav";
 
 const Header = () => {
   return (
-    <header className="py-6">
+    <header
+      className="py-6"
+      data-aos="fade-down"
+      data-aos-duration="2000"
+      data-aos-delay="900"
+    >
       <div className="">
         <div className="flex items-center justify-between">
           {/* logo */}
