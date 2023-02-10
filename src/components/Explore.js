8792bf2649b417baa13a7abfa90e3feddd8b2eba
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-10 16:59:22 +0200

    New Update

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -4,18 +4,24 @@ const Explore = () => {
   return (
     <section className="min-h-[400px] mb-16 lg:mb-4">
       <div className="container mx-auto h-full">
-        <div className="h-full bg-explore bg-no-repeat
+        <div
+          className="h-full bg-explore bg-no-repeat
         bg-cover p-14 flex flex-col items-start
-        justify-center">
-          <h3 className="text-3xl font-semibold mb-8">Explore product in new way</h3>
+        justify-center"
+        >
+          <h3 className="text-3xl font-semibold mb-8">
+            Explore product in new way
+          </h3>
           <p className="max-w-xs mb-12">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque
             lobortis fringilla ante vel ornare. Nunc luctus lectus ut dui
             tristique.
           </p>
           {/* Form */}
-          <form className="flex flex-col w-full space-y-4
-          gap-x-4 lg:flex-row lg:space-y-0">
+          <form
+            className="flex flex-col w-full space-y-4
+          gap-x-4 lg:flex-row lg:space-y-0"
+          >
             <input
               className="bg-gradient-to-t from-[#341D38]
             to-[#271223] h-12 px-4 outline-none
