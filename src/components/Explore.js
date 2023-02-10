7b4ba476e17ea900d81bf964dc122734aa3e0a6f
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-10 16:55:18 +0200

    New Update

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React from "react";
 
 const Explore = () => {
-  return <div>Explore</div>;
+  return (
+    <section className="min-h-[400px] mb-16 lg:mb-4">
+      <div className="container mx-auto h-full">
+        <div className="h-full bg-explore bg-no-repeat
+        bg-cover p-14 flex flex-col items-start
+        ">
+          <h3>Explore product in new way</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque
+            lobortis fringilla ante vel ornare. Nunc luctus lectus ut dui
+            tristique.
+          </p>
+          {/* Form */}
+          <form className="flex flex-col w-full space-y-4
+          gap-x-4 lg:flex-row lg:space-y-0">
+            <input
+              className="bg-gradient-to-t from-[#341D38]
+            to-[#271223] h-12 px-4 outline-none
+            rounded-md"
+              type="text"
+              placeholder="Your email"
+            />
+            <button className="btn">Start</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Explore;
