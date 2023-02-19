5be53875eb08a08dccb89c0e8bee45d60ac0a277
Ahmedelsayedd01 <ahmedelsayed201972@gmail.com>
2023-02-19 21:26:04 +0200

    Last Update

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="container mx-auto -mt-14 h-full">
+      <div
+        className="text-center"
+        data-aos="fade-left"
+        data-aos-duration="1200"
+        data-aos-offset="900"
+      >
+        Copyright <span className="text-[#00d9ff]">&copy;</span> 2023 Vr Light.
+        All rights reserved.
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
